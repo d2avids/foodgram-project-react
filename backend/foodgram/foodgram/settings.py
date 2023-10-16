ad5c1d344c45477f103202bf1b63ce459e695174
David Saidov <delightxxls@gmail.com>
2023-10-17 02:36:48 +0300

    fix config

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = f'{ALLOWED_HOSTS[0]}/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
